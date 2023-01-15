4f1da2323ad5555822cd9f2c3f357f319f4dbe84
RaysonYT <nikolomoec@gmail.com>
2023-01-15 18:54:08 +0200

    Initial Commit

---
+++ CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by Nikita Kolomoec on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
